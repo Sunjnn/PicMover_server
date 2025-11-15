0e8bdf2ea0e4e4b33bd095083db6c6729b524d26
sunjnn <sun.jianan@outlook.com>
2025-11-15 22:28:34 +0800

    feat: redirect Qt logs to file in executable directory

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,45 @@
-#include <QApplication>
+#include <qapplication.h>
+#include <qdir.h>
 
 #include "config.hxx"
 #include "main_window.hxx"
 
+void custom_message_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    static QFile file(QCoreApplication::applicationDirPath() + "/PicMover_log.txt");
+
+    if (!file.isOpen()) {
+        file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text);
+    }
+
+    QTextStream out(&file);
+    out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ");
+
+    switch (type) {
+    case QtDebugMsg:
+        out << "[DEBUG] ";
+        break;
+    case QtInfoMsg:
+        out << "[INFO] ";
+        break;
+    case QtWarningMsg:
+        out << "[WARN] ";
+        break;
+    case QtCriticalMsg:
+        out << "[CRIT] ";
+        break;
+    case QtFatalMsg:
+        out << "[FATAL] ";
+        break;
+    }
+
+    out << msg << "\n";
+}
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName(APPLICATION_NAME);
+
+    qInstallMessageHandler(custom_message_handler);
 
     MainWindow mainWindow;
     mainWindow.show();
